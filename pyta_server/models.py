0629617bc5ae120464a9785e5395954ceb5e4b9c
Adam Gleizer <adam.gl@hotmail.com>
2020-03-23 23:54:41 -0400

    id POST issue resolved, minor fixes

--- pyta_server/models.py
+++ pyta_server/models.py
@@ -5,7 +5,7 @@ class Uploads(db.Model):
     primary = db.Column(db.Integer, primary_key=True)
     source = db.Column(db.String(200), nullable=False)
     config = db.Column(db.String(200), nullable=False)
-    identifier = db.Column(db.String(200), nullable=False)
+    identifier = db.Column(db.String(200), server_default="no-id", nullable=False)
     upload_time = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     def __repr__(self):
