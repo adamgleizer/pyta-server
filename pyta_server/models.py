6f047d79da095a39249b2baee0da4f1b63a5842f
Adam Gleizer <adam.gl@hotmail.com>
2020-04-10 00:25:16 -0400

    Changed db design and updated routes.py accordingly

--- pyta_server/models.py
+++ pyta_server/models.py
@@ -1,12 +1,18 @@
 from pyta_server import db
 from sqlalchemy.sql import func
 
+class Submissions(db.Model):
+    identifier = db.Column(db.String(200), server_default="no-id", primary_key=True)
+    submit_time = db.Column(db.DateTime(timezone=True), server_default=func.now(), primary_key=True)
+    uploads = db.relationship('Uploads', backref='submission', lazy=True)
+    config = db.Column(db.String(200), nullable=False)
+
 class Uploads(db.Model):
     primary = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(200), nullable=False)
+    upload_time = db.Column(db.DateTime(timezone=True), nullable=False)
     source = db.Column(db.String(200), nullable=False)
-    config = db.Column(db.String(200), nullable=False)
-    identifier = db.Column(db.String(200), server_default="no-id", nullable=False)
-    upload_time = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
+    __table_args__ = (db.ForeignKeyConstraint([user_id, upload_time],
+                                           [Submissions.identifier, Submissions.submit_time]),
+                      {})
 
-    def __repr__(self):
-        return f"<Uploads(source={self.source}, config={self.config})>"
--- pyta_server/routes.py
+++ pyta_server/routes.py
@@ -3,30 +3,39 @@ import secrets
 import psycopg2
 from flask import request
 from pyta_server import app, db
-from pyta_server.models import Uploads
+from pyta_server.models import Submissions, Uploads
 
 @app.route('/', methods=['POST'])
 def receive():
     rand_hex = secrets.token_hex(8)
-    src_f = request.files.get('source')
+    src_f = request.files
     cfg_f = request.files.get('config')
-    id = request.form.get('id')
+    ident = request.form.get('id')
+    f_paths = []
     if src_f is not None and cfg_f is not None:
-        _, src_ext = os.path.splitext(src_f.filename)
+        for f in list(src_f.values())[:-1]: # Last value is cfg, handled separately
+            _, src_ext = os.path.splitext(f.filename)
+            src_n = rand_hex + src_ext
+            src_path = os.path.join(app.root_path, 'static', 'source', src_n)
+            f_paths.append(src_path)
+            f.save(src_path)
+            rand_hex = secrets.token_hex(8)
+
         _, cfg_ext = os.path.splitext(cfg_f.filename)
-        src_n = rand_hex + src_ext
         cfg_n = rand_hex + cfg_ext
-        src_path = os.path.join(app.root_path, 'static', 'source', src_n)
         cfg_path = os.path.join(app.root_path, 'static', 'config', cfg_n)
-        src_f.save(src_path)
         cfg_f.save(cfg_path)
 
-        upload = Uploads(source=src_path, config=cfg_path, identifier=id)
-        try:
-            db.session.add(upload)
-            db.session.commit()
-            return "files successfully received"
-        except (Exception, psycopg2.Error) as error:
-            print("Error while connecting to PostgreSQL", error)
+        submission = Submissions(identifier=ident, config=cfg_path)
+        for path in f_paths:
+            upload = Uploads(source=path, submission=submission)
+            try:
+                db.session.add(upload)
+                db.session.commit()
+
+            except (Exception, psycopg2.Error) as error:
+                print("Error while connecting to PostgreSQL", error)
+
+        return "files successfully received"
 
     return "bad request"
