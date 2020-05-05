88549696fdde22d6290a60eec1beb8e0860bb0c0
Adam Gleizer <adam.gl@hotmail.com>
2020-05-05 11:02:15 -0400

    Simplified db design, cleaned up handling config in routes.py

--- pyta_server/models.py
+++ pyta_server/models.py
@@ -2,17 +2,14 @@ from pyta_server import db
 from sqlalchemy.sql import func
 
 class Submissions(db.Model):
-    identifier = db.Column(db.String(200), server_default="no-id", primary_key=True)
-    submit_time = db.Column(db.DateTime(timezone=True), server_default=func.now(), primary_key=True)
+    device_uuid = db.Column(db.String(200), server_default="no-id", primary_key=True)
     uploads = db.relationship('Uploads', backref='submission', lazy=True)
-    config = db.Column(db.String(200), nullable=False)
+    version = db.Column(db.String(200), nullable=False)
 
 class Uploads(db.Model):
     primary = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(200), nullable=False)
-    upload_time = db.Column(db.DateTime(timezone=True), nullable=False)
+    upload_time = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     source = db.Column(db.String(200), nullable=False)
-    __table_args__ = (db.ForeignKeyConstraint([user_id, upload_time],
-                                           [Submissions.identifier, Submissions.submit_time]),
-                      {})
+    config = db.Column(db.String(200), nullable=True)
+    submission_id = db.Column(db.String(200), db.ForeignKey('submissions.device_uuid'), nullable=False)
 
--- pyta_server/routes.py
+++ pyta_server/routes.py
@@ -5,36 +5,41 @@ from flask import request
 from pyta_server import app, db
 from pyta_server.models import Submissions, Uploads
 
+def commit_upload(upload):
+        try:
+            db.session.add(upload)
+            db.session.commit()
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+
 @app.route('/', methods=['POST'])
 def receive():
-    rand_hex = secrets.token_hex(8)
-    src_f = request.files
-    cfg_f = request.files.get('config')
-    ident = request.form.get('id')
-    f_paths = []
-    if src_f is not None and cfg_f is not None:
-        for f in list(src_f.values())[:-1]: # Last value is cfg, handled separately
+    if isinstance(request.files, dict):
+        uuid = request.form.get('id')
+        submission = Submissions(device_uuid=uuid, version='current') #Not sure how to find current version from __init__, ask David
+        src_f = {k:v for k,v in request.files.items() if k != 'config'} #Excluding potential config file
+        cfg_f = request.files.get('config')
+        f_paths = []
+        for f in list(src_f.values()):
+            rand_hex = secrets.token_hex(8)
             _, src_ext = os.path.splitext(f.filename)
             src_n = rand_hex + src_ext
             src_path = os.path.join(app.root_path, 'static', 'source', src_n)
             f_paths.append(src_path)
             f.save(src_path)
+        if cfg_f: # Non-default config file
             rand_hex = secrets.token_hex(8)
-
-        _, cfg_ext = os.path.splitext(cfg_f.filename)
-        cfg_n = rand_hex + cfg_ext
-        cfg_path = os.path.join(app.root_path, 'static', 'config', cfg_n)
-        cfg_f.save(cfg_path)
-
-        submission = Submissions(identifier=ident, config=cfg_path)
-        for path in f_paths:
-            upload = Uploads(source=path, submission=submission)
-            try:
-                db.session.add(upload)
-                db.session.commit()
-
-            except (Exception, psycopg2.Error) as error:
-                print("Error while connecting to PostgreSQL", error)
+            _, cfg_ext = os.path.splitext(cfg_f.filename)
+            cfg_n = rand_hex + cfg_ext
+            cfg_path = os.path.join(app.root_path, 'static', 'config', cfg_n)
+            cfg_f.save(cfg_path)
+            for path in f_paths:
+                upload = Uploads(source=path, config=cfg_path, submission=submission)
+                commit_upload(upload)
+        else: #Default config was used
+            for path in f_paths:
+                upload = Uploads(source=path, submission=submission)
+                commit_upload(upload)
 
         return "files successfully received"
 
