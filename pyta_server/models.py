aebeb8015bd73b0ed00a56cf8cd61201a66eda91
Adam Gleizer <adam.gl@hotmail.com>
2020-05-15 17:33:20 -0400

    Changed directory structure, hashed mac addresses

--- pyta_server/models.py
+++ pyta_server/models.py
@@ -1,15 +1,15 @@
 from pyta_server import db
 from sqlalchemy.sql import func
 
-class Submissions(db.Model):
-    device_uuid = db.Column(db.String(200), server_default="no-id", primary_key=True)
-    uploads = db.relationship('Uploads', backref='submission', lazy=True)
-    version = db.Column(db.String(200), nullable=False)
+class Devices(db.Model):
+    device_uuid = db.Column(db.String(800), server_default="no-id", primary_key=True)
+    uploads = db.relationship('Uploads', backref='device', lazy=True)
+    version = db.Column(db.String(800), nullable=False)
 
 class Uploads(db.Model):
     primary = db.Column(db.Integer, primary_key=True)
     upload_time = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
-    source = db.Column(db.String(200), nullable=False)
-    config = db.Column(db.String(200), nullable=True)
-    submission_id = db.Column(db.String(200), db.ForeignKey('submissions.device_uuid'), nullable=False)
+    source = db.Column(db.String(800), nullable=False)
+    config = db.Column(db.String(800), nullable=True)
+    device_id = db.Column(db.String(800), db.ForeignKey('devices.device_uuid'), nullable=False)
 
--- pyta_server/routes.py
+++ pyta_server/routes.py
@@ -3,7 +3,7 @@ import secrets
 import psycopg2
 from flask import request
 from pyta_server import app, db
-from pyta_server.models import Submissions, Uploads
+from pyta_server.models import Devices, Uploads
 
 def commit_upload(upload):
         try:
@@ -15,8 +15,12 @@ def commit_upload(upload):
 @app.route('/', methods=['POST'])
 def receive():
     if isinstance(request.files, dict):
-        uuid = request.form.get('id')
-        submission = Submissions(device_uuid=uuid, version='current') #Not sure how to find current version from __init__, ask David
+        unique_id = request.form.get('id')
+        version = request.form.get('version')
+        if db.session.query(Devices).get(unique_id):
+            device = db.session.query(Devices).get(unique_id)
+        else:
+            device = Devices(device_uuid=unique_id, version=version)
         src_f = {k:v for k,v in request.files.items() if k != 'config'} #Excluding potential config file
         cfg_f = request.files.get('config')
         f_paths = []
@@ -24,21 +28,27 @@ def receive():
             rand_hex = secrets.token_hex(8)
             _, src_ext = os.path.splitext(f.filename)
             src_n = rand_hex + src_ext
-            src_path = os.path.join(app.root_path, 'static', 'source', src_n)
-            f_paths.append(src_path)
-            f.save(src_path)
+            src_path = os.path.join(app.root_path, 'static', 'source', unique_id)
+            if not os.path.exists(src_path):
+                os.makedirs(src_path)
+            file_loc = os.path.join(src_path, src_n)
+            f_paths.append(file_loc)
+            f.save(file_loc)
         if cfg_f: # Non-default config file
             rand_hex = secrets.token_hex(8)
             _, cfg_ext = os.path.splitext(cfg_f.filename)
             cfg_n = rand_hex + cfg_ext
-            cfg_path = os.path.join(app.root_path, 'static', 'config', cfg_n)
-            cfg_f.save(cfg_path)
+            cfg_path = os.path.join(app.root_path, 'static', 'config', unique_id)
+            if not os.path.exists(cfg_path):
+                os.makedirs(cfg_path)
+            cfg_loc = os.path.join(cfg_path, cfg_n)
+            cfg_f.save(cfg_loc)
             for path in f_paths:
-                upload = Uploads(source=path, config=cfg_path, submission=submission)
+                upload = Uploads(source=path, config=cfg_path, device=device)
                 commit_upload(upload)
         else: #Default config was used
             for path in f_paths:
-                upload = Uploads(source=path, submission=submission)
+                upload = Uploads(source=path, device=device)
                 commit_upload(upload)
 
         return "files successfully received"
