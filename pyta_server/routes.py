73f2c8dc75537b251a5dcb073e30ce9ad80582af
Adam Gleizer <adam.gl@hotmail.com>
2020-07-13 13:57:38 -0400

    Receives and saves config as JSON

--- pyta_server/routes.py
+++ pyta_server/routes.py
@@ -2,9 +2,14 @@ import os
 import secrets
 import psycopg2
 import json
+import datetime
 from flask import request
 from pyta_server import app, db
 from pyta_server.models import Devices, Uploads, Files, Errors
+from typing import Dict
+
+INIT_SRC_PATH = os.path.join(app.root_path, 'static', 'source')
+INIT_CFG_PATH = os.path.join(app.root_path, 'static', 'config')
 
 
 def update_db(entry):
@@ -16,7 +21,7 @@ def update_db(entry):
         print("Error while connecting to PostgreSQL", error)
 
 
-def commit_errors(error_info, upload, file=None):
+def commit_errors(error_info: Dict[str, str], upload, file=None):
     """Make an Errors tuple and save it to the database."""
     error = Errors(upload=upload,
                    file=file,
@@ -31,55 +36,55 @@ def commit_errors(error_info, upload, file=None):
 @app.route('/', methods=['POST'])
 def receive():
     """Receive POST request sent from user running PyTA."""
-    if isinstance(request.files, dict):
-        upload_time = request.form.get('time')  # Replace ':' with "-" because : is not a valid directory character
-        time_stamp = upload_time.replace(":", "-")
-        unique_id = request.form.get('id')
-        version = request.form.get('version')
-        errors = json.loads(request.form.get('errors'))
+    upload_time = str(datetime.datetime.utcnow())
+    time_stamp = upload_time.replace(":", "-")  # Replace ':' with "-" because : is not a valid directory character
+    unique_id = request.form.get('id')
+    version = request.form.get('version')
 
-        if db.session.query(Devices).get(unique_id):  # Check if device already exists in database
-            device = db.session.query(Devices).get(unique_id)
-        else:
-            device = Devices(device_uuid=unique_id, version=version)
-            update_db(device)
+    if db.session.query(Devices).get(unique_id):  # Check if device already exists in database
+        device = db.session.query(Devices).get(unique_id)
+    else:
+        device = Devices(device_uuid=unique_id, version=version)
+        update_db(device)
 
-        src_f = {k: v for k, v in request.files.items() if k != 'config'}  # Exclude potential config file
-        cfg_f = request.files.get('config')
-        f_paths = []
-        cfg_loc = None  # initialized at None for when default config was used
-        for f in list(src_f.values()):  # Saving files
-            src_path = os.path.join(app.root_path, 'static', 'source', unique_id, time_stamp)
-            if not os.path.exists(src_path):
-                os.makedirs(src_path)
-            file_loc = os.path.join(src_path, f.filename)
-            f_paths.append(file_loc)
-            f.save(file_loc)
-        if cfg_f:  # Saving if non-default config file was used
-            rand_hex = secrets.token_hex(8)
-            _, cfg_ext = os.path.splitext(cfg_f.filename)
-            cfg_n = rand_hex + cfg_ext
-            cfg_path = os.path.join(app.root_path, 'static', 'config', unique_id, time_stamp)
-            if not os.path.exists(cfg_path):
-                os.makedirs(cfg_path)
-            cfg_loc = os.path.join(cfg_path, cfg_n)
-            cfg_f.save(cfg_loc)
+    src_files = request.files.values()
+    payload = json.loads(request.form.get('payload'))
+    errors = payload.get('errors')
+    cfg = payload.get('cfg')
+    f_paths = []
+    cfg_loc = None  # initialized at None for when default config was used
+    for f in src_files:  # Saving files
+        src_path = os.path.join(INIT_SRC_PATH, unique_id, time_stamp)
+        if not os.path.exists(src_path):
+            os.makedirs(src_path)
+        file_loc = os.path.join(src_path, f.filename)
+        f_paths.append(file_loc)
+        f.save(file_loc)
 
-        upload = Uploads(upload_time=upload_time, config=cfg_loc, device=device)
-        if f_paths:  # User uploaded files along with errors
-            for path in f_paths:
-                file = Files(upload=upload, path=path)
-                update_db(file)
-                for error_codes in errors.values():
-                    for error_info in error_codes:
-                        # Matching errors to files they were present in
-                        if (error_info.get('module') + '.py') == os.path.basename(os.path.normpath(path)):
-                            commit_errors(error_info=error_info, upload=upload, file=file)
-        else:  # User uploaded just errors
-            for error_list in errors.values():
-                for error_info in error_list:
-                    commit_errors(error_info=error_info, upload=upload)
+    if cfg:  # Saving if non-default config file was used
+        rand_hex = secrets.token_hex(8)
+        cfg_n = rand_hex + '.json'
+        cfg_path = os.path.join(INIT_CFG_PATH, unique_id, time_stamp)
+        if not os.path.exists(cfg_path):
+            os.makedirs(cfg_path)
+        cfg_loc = os.path.join(cfg_path, cfg_n)
+        cfg_f = open(cfg_loc, 'w')
+        json.dump(cfg, cfg_f, indent=4)
+        cfg_f.close()
 
-        return "files successfully received"
+    upload = Uploads(upload_time=upload_time, config=cfg_loc, device=device)
+    if f_paths:  # User uploaded files along with errors
+        for path in f_paths:
+            file = Files(upload=upload, path=path)
+            update_db(file)
+            for error_codes in errors.values():
+                for error_info in error_codes:
+                    # Matching errors to files they were present in
+                    if (error_info.get('module') + '.py') == os.path.basename(os.path.normpath(path)):
+                        commit_errors(error_info=error_info, upload=upload, file=file)
+    else:  # User uploaded just errors
+        for error_list in errors.values():
+            for error_info in error_list:
+                commit_errors(error_info=error_info, upload=upload)
 
-    return "bad request"
+    return "files successfully received"
