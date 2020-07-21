cfa97a566950cbb23f34f90d4dfc4942b69769be
Adam Gleizer <adam.gl@hotmail.com>
2020-07-21 15:31:50 -0400

    Enumerates duplicate file names before saving

--- pyta_server/routes.py
+++ pyta_server/routes.py
@@ -3,6 +3,8 @@ import secrets
 import psycopg2
 import json
 import datetime
+import re
+
 from flask import request
 from pyta_server import app, db
 from pyta_server.models import Devices, Uploads, Files, Errors
@@ -10,27 +12,6 @@ from typing import Dict
 
 INIT_SRC_PATH = os.path.join(app.root_path, 'static', 'source')
 INIT_CFG_PATH = os.path.join(app.root_path, 'static', 'config')
-
-
-def update_db(entry):
-    """Save the entry to the database."""
-    try:
-        db.session.add(entry)
-        db.session.commit()
-    except (Exception, psycopg2.Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-
-
-def commit_errors(error_info: Dict[str, str], upload, file=None):
-    """Make an Errors tuple and save it to the database."""
-    error = Errors(upload=upload,
-                   file=file,
-                   msg_id=error_info.get('msg_id'),
-                   msg=error_info.get('msg'),
-                   symbol=error_info.get('symbol'),
-                   category=error_info.get('category'),
-                   line=error_info.get('line'))
-    update_db(error)
 
 
 @app.route('/', methods=['POST'])
@@ -51,13 +32,16 @@ def receive():
     payload = json.loads(request.form.get('payload'))
     errors = payload.get('errors')
     cfg = payload.get('cfg')
-    f_paths = []
     cfg_loc = None  # initialized at None for when default config was used
+    f_paths = []
     for f in src_files:  # Saving files
         src_path = os.path.join(INIT_SRC_PATH, unique_id, time_stamp)
         if not os.path.exists(src_path):
             os.makedirs(src_path)
-        file_loc = os.path.join(src_path, f.filename)
+        curr_name = f.filename
+        file_loc = os.path.join(src_path, curr_name)
+        while os.path.exists(file_loc):
+            file_loc = os.path.join(src_path, enumerate_file_name(curr_name))
         f_paths.append(file_loc)
         f.save(file_loc)
 
@@ -73,10 +57,11 @@ def receive():
         cfg_f.close()
 
     upload = Uploads(upload_time=upload_time, config=cfg_loc, device=device)
-    if f_paths:  # User uploaded files along with errors
+    if f_paths:  # User uploaded files
         for path in f_paths:
             file = Files(upload=upload, path=path)
             update_db(file)
+            # 'errors' is an empty dict if the User only uploaded files
             for error_codes in errors.values():
                 for error_info in error_codes:
                     # Matching errors to files they were present in
@@ -87,4 +72,37 @@ def receive():
             for error_info in error_list:
                 commit_errors(error_info=error_info, upload=upload)
 
-    return "files successfully received"
+    return "Data submission successfully received"
+
+
+def update_db(entry) -> None:
+    """Save the entry to the database."""
+    try:
+        db.session.add(entry)
+        db.session.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
+
+def commit_errors(error_info: Dict[str, str], upload, file=None) -> None:
+    """Make an Errors tuple and save it to the database."""
+    error = Errors(upload=upload,
+                   file=file,
+                   msg_id=error_info.get('msg_id'),
+                   msg=error_info.get('msg'),
+                   symbol=error_info.get('symbol'),
+                   category=error_info.get('category'),
+                   line=error_info.get('line'))
+    update_db(error)
+
+
+def enumerate_file_name(filename: str) -> str:
+    pattern = re.compile("(\d+)")
+    # Counter is the '(n)' portion of an enumerated filename
+    f_name, counter = filename[:-3], filename[-3:]
+    if pattern.match(counter):
+        curr_count = int(counter[1])
+        new_enum = " (" + str(curr_count + 1) + ").py"
+        return f_name + new_enum
+    else:
+        return f_name + " (1).py"
